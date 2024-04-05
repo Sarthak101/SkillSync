646674d17bee85161f9b91ab95e6ac35ead6cd82
annelizshoney04 <135529873+annelizshoney04@users.noreply.github.com>
2024-04-05 12:56:04 +0530

    NGO dashboard UI

---
+++ php/NGO/dashboard.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../../css/dash.css">
+    <link rel="icon" type="image/x-icon" href="../../public/Icons/Bgtp.ico">
+
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>SkillSync</title>
+</head>
+
+<body>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="../../public/Icons/logo.png" alt="logo">
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">SkillSync</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+
+                <ul class="menu-links">
+                    <li class="nav-link" >
+                        <a href="#" onclick="loadIframe('dash.php')">
+                            <i class='bx bx-home-alt icon'></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                        <span class="tooltip">Dashboard</span>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#" onclick="loadIframe('request.php')">
+                            <i class='bx bx-bar-chart-alt-2 icon'></i>
+                            <span class="text nav-text">Jobs</span>
+                        </a>
+                        <span class="tooltip">Jobs</span>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#" onclick="loadIframe('work.php')">
+                            <i class='bx bx-chat icon'></i>
+                            <span class="text nav-text">Employees</span>
+                        </a>
+                        <span class="tooltip">Check</span>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#" onclick="loadIframe('workreport.php')">
+                            <i class='bx bx-dollar-circle icon'></i>
+                            <span class="text nav-text">Work Report</span>
+                        </a>
+                        <span class="tooltip">Work Report</span>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#" onclick="loadIframe('settings.php')">
+                            <i class='bx bx-cog icon'></i>
+                            <span class="text nav-text">Settings</span>
+                        </a>
+                        <span class="tooltip">Settings</span>
+                    </li>
+
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="../logout.php">
+                        <i class='bx bx-log-out icon'></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                    <span class="tooltip">Logout</span>
+                </li>
+
+                <li class="mode">
+                    <!-- <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark mode</span> -->
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+
+        <div id = "frame" ></div>
+    </section>
+
+    <script>
+        const body = document.querySelector('body'),
+            sidebar = body.querySelector('nav'),
+            toggle = body.querySelector(".toggle"),
+            modeSwitch = body.querySelector(".toggle-switch"),
+            modeText = body.querySelector(".mode-text");
+
+
+        toggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+        })
+
+        modeSwitch.addEventListener("click", () => {
+            body.classList.toggle("dark");
+
+            if (body.classList.contains("dark")) {
+                modeText.innerText = "Light mode";
+            } else {
+                modeText.innerText = "Dark mode";
+
+            }
+        });
+        loadIframe('dash.php'); //Default Loaded Dashboard
+        function loadIframe(src) {
+        removeIframe();
+        var iframeContainer = document.getElementById('frame');
+
+        iframeContainer.innerHTML = '<iframe id="framenew" src="' + src + '"width=100% frameborder="0"  "></iframe>';
+    }
+    
+        function removeIframe() {
+            var iframeContainer = document.getElementById('frame');
+            iframeContainer.innerHTML = ''; // Remove the iframe by clearing the container
+
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ php/NGO/includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>
+  <?php echo TITLE ?>
+ </title>
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="../../css/bootstrap.min.css">
+
+ <!-- Font Awesome CSS -->
+ <link rel="stylesheet" href="../../css/all.min.css">
+
+ <!-- Custome CSS -->
+ <link rel="stylesheet" href="../../css/custom.css">
+
+</head>
+
+<body>
+ <!-- Top Navbar -->
+ <!-- <nav class="navbar navbar-dark fixed-top bg-danger p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="dash.php">Welcome User!</a>
+ </nav> -->
+
+ <!-- Side Bar -->
+ <div class="container-fluid mb-5" style="margin-top:40px;">
+  <div class="row">
+   <nav class="col-sm-0 col-md-1 sidebar py-5 d-print-none">
+    
+   </nav>
